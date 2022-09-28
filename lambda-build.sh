16a6fac0a1ecaccefbef8e6bc82a6e587ae243d4
Andy Vo <47099119+andyqvo@users.noreply.github.com>
2022-09-28 09:13:26 -0700

    updated bash

--- lambda-build.sh
+++ lambda-build.sh
@@ -7,6 +7,6 @@ echo "Removing any previous build(s)/artifacts"
 pip install -r requirements.txt -t build/
 cp baseball.py build/
 
-# echo "Zipping contents"
-# cd ./build
-# zip -r lambda_handler.zip ./*
+echo "Zipping contents"
+cd ./build
+zip -r lambda_handler.zip ./*
