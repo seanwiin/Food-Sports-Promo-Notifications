2d2dac6c7e32f3042ce980bb3d9eb2b58ba02a0f
Andy Vo <47099119+andyqvo@users.noreply.github.com>
2022-09-28 09:13:07 -0700

    added bash and gitignore

---
+++ lambda-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building .zip for Lambda"
+
+echo "Removing any previous build(s)/artifacts"
+
+pip install -r requirements.txt -t build/
+cp baseball.py build/
+
+# echo "Zipping contents"
+# cd ./build
+# zip -r lambda_handler.zip ./*
